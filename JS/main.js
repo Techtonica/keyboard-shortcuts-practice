744e71f66b3f9f6ea1f169dc0e32a8bb8a0d4821
Deepti Garg <deepti7garg@gmail.com>
2018-02-02 17:18:41 -0800

    Updated JS file

--- JS/main.js
+++ JS/main.js
@@ -121,7 +121,7 @@ function readText(){
 
 function writeQuestion(question, speed, i) {
   if (i < question.length) {
-    $("#textdiv").innerHTML += question.charAt(i);
+    $("#textdiv").text() += question.charAt(i);
     i++;
     setTimeout(writeQuestion, speed);
   }
