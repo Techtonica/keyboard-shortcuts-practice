8c77d85dc360541f5ce0839a77728f663923306f
tburke <tburke@switchfly.com>
2018-10-20 15:58:56 -0700

    set up navbar, add requirements file

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
+from datetime import datetime
+
 from app import db, login
 
 @login.user_loader
@@ -13,6 +15,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    # progress = db.relationship("Progress", backref="user", lazy="dynamic")
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -23,3 +26,11 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+# class Progress(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     pub_date = db.Column(db.DateTime, nullable=False,
+#                          default=datetime.utcnow)
+#     user = db.relationship('User')
+#     # TODO calculate speed metric
+#     # speed =
