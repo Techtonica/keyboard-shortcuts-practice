1a642f3ce4a136ec3b325beec781f4ab97b18935
ashley-g <69818337+ashley-g@users.noreply.github.com>
2020-10-01 14:48:58 -0400

    34  caps lock toggle (#98)
    * fixed caps lock sync issue, capslock on is a keydown and capslock off is a keyup event
    
    * deleted unecesseray if condition in handle function for capslock

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -95,11 +95,6 @@ function handle(e) {
     $("#"+e.key.toLowerCase()).toggleClass("pressed");
     $('.letter').toggleClass('uppercase');
   }
-  else if(e.key.toLowerCase()=="capslock" && caps==true) {
-    $("#"+e.key.toLowerCase()).toggleClass("pressed");
-    $('.letter').toggleClass('uppercase');
-    caps=false;
-  }
   else $("#"+e.key.toLowerCase() ).addClass("pressed");
 }
 
@@ -120,7 +115,11 @@ function release(e) {
   if(e.code.toLowerCase()=="space"){
     $("#space").removeClass("pressed");
   }
-  if(e.key.toLowerCase()=="capslock") return
+  if(e.key.toLowerCase()=="capslock"){
+    $("#"+e.key.toLowerCase()).toggleClass("pressed");
+    $('.letter').toggleClass('uppercase');
+    caps=false;
+  } 
   else{
     $("#"+e.key.toLowerCase() ).removeClass("pressed");
   }
