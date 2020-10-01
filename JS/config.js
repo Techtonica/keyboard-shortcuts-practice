4efcea91aaf1c8fa7dd01e2c34031c1f40cabfbb
Bill DePhillips <355438+vegetabill@users.noreply.github.com>
2020-09-30 18:56:44 -0700

    Resolves #65 - Finish ORM Setup (#91)
    * Fixes #65 - Finish ORM Setup
    - Ensure we can connect to the DB before launching Express
    - Use sqlite for normal dev mode; postgres for production
    - Upgrade sequelize and postgres npm packages since the versions we were using hung trying to connect to pg
    - Use the sequelize.sync functionality to automatically setup any table(s)
    
    * Allow either type of DB in development mode
    - in dev mode, have default DB_URL of sqlite
    - in prod, DB_URL must be specified
    
    * fix .gitignore to have correct sqlite db name
    
    * clean up package.json scripts

--- JS/config.js
+++ JS/config.js
@@ -1,1 +1,19 @@
-exports.URL = process.env.URL;
+require("dotenv").config();
+
+const { DB_URL, NODE_ENV } = process.env;
+
+const isProduction = () => NODE_ENV === "production";
+const isDevelopment = () => NODE_ENV === "development";
+
+const getDefaultDbUrl = () => {
+  if (isDevelopment()) {
+    return "sqlite:dev-db.sqlite";
+  }
+  throw new Error("DB_URL must be specified in production mode");
+};
+
+module.exports = {
+  isProduction,
+  isDevelopment,
+  DB_URL: DB_URL || getDefaultDbUrl(),
+};
--- JS/orm.js
+++ JS/orm.js
@@ -1,31 +1,34 @@
-// access config/env variables
-//var config = require("./config.js");
-require('dotenv').config();
-console.log(`${process.env.URL}`);
+const Sequelize = require("sequelize");
+const { DB_URL } = require("./config.js");
 
-// setting up ORM
-var Sequelize = require('sequelize');
-var sequelize = new Sequelize(`${process.env.URL}`);
-
+const sequelize = new Sequelize(DB_URL);
 
 // define basic model for a user
 const Model = Sequelize.Model;
 class User extends Model {}
-User.init({
-  uid: Sequelize.INTEGER,
-  q_number: Sequelize.STRING,
-  last_login: Sequelize.DATE
-}, { sequelize, modelName: 'user' });
+User.init(
+  {
+    q_number: Sequelize.STRING,
+    last_login: Sequelize.DATE,
+    id: {
+      type: Sequelize.STRING,
+      primaryKey: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: "user",
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+  }
+);
 
-// testing database connection
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-});
+const connectToDb = async () => {
+  await sequelize.authenticate();
+  await sequelize.sync();
+};
 
-// TODO: make User model accessible from other files
-// export { User };
+module.exports = {
+  User,
+  connectToDb,
+};
