001c5b22c6d8b8a5820e0726718372a3c5137da4
Josseline Perdomo <josselineperdomoc@gmail.com>
2020-10-01 19:20:44 -0400

    added find by user_id validation

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const{ UserAnswers } = require('../JS/orm');
+const{ User, UserAnswers } = require('../JS/orm');
 
 router.get('/', (req, res) => {
     res.render('index');
@@ -15,13 +15,23 @@ router.post('/user/answers/question/:questionNumber', (req, res) => {
     if(req.body.id) {
 		return res.status(400).json({'message': 'ID should not be provided'});
     }    
-    UserAnswers.create({
-        user_id: req.body.userId,
-        question_number: req.params.questionNumber,
-        is_correct: req.body.isCorrect,
-        elapsed_time_ms: req.body.elapsedTimeMs
-    }).then(userAnswer => {
-        return res.status(201).json(userAnswer)
+    // Created won't be needed when issue #74 has been solved, should be changed by findOne and handle the errors
+    User.findOrCreate({ 
+        where: {id: req.body.userId}
+    }).then(users => {
+        let user = users[0];
+        
+        UserAnswers.create({
+            user_id: user.id,
+            question_number: req.params.questionNumber,
+            is_correct: req.body.isCorrect,
+            elapsed_time_ms: req.body.elapsedTimeMs
+        }).then(userAnswer => {
+            return res.status(201).json(userAnswer)
+        }).catch(error => {
+            console.log(error);
+            return res.status(400).json(error.errors)
+        })
     }).catch(error => {
         console.log(error);
         return res.status(400).json(error.errors)
