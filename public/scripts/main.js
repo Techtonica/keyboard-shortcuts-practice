2e06d3ad9da6e1475ca50e145949678e33ba930e
Beleicia Bullock <10715618+belebull@users.noreply.github.com>
2020-10-01 19:11:31 -0500

    feat: add shortcut type tag to JSON object and main screen (#103)

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -172,6 +172,7 @@ function readText(){
   if(quesNo!=null){
     commandText = allData[parseInt(quesNo)-1].answer
     answerkeys = allData[parseInt(quesNo)-1].keys
+    type = allData[parseInt(quesNo) - 1].shortcutType
     //commandText = "A+Control"  //$("#textdiv").text(); // Will be taken from some other list type of a source.
     //Each command will have an associated question text used in writeQuestion
     var speed = 50
@@ -185,6 +186,14 @@ function readText(){
       // Highlight the prompt keys
       promptKey2(val)
     });
+
+    // update shortcut type
+    $('#shortcut-tag').first().text(type + ' Shortcut')
+    if(type == 'mac') {
+      $('#shortcut-tag').first().css('background-color', '#3455db')
+    } else {
+      $('#shortcut-tag').first().css('background-color', '#4b2142')
+    }
 
     /* commandText.split('+').forEach(function(c) {
       if(c.toLowerCase()=="command"){
